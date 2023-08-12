922d8083a0dfa43d916eb1720c179b65422bf8af
Ciel-azure <greenavc@gmail.com>
2023-08-13 00:05:39 +0900

    POJO to Spring Boot API

--- src/main/java/com/example/demo/ProductAdapter.java
+++ src/main/java/com/example/demo/ProductAdapter.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Component;
+
+@Component
 class ProductAdapter implements ProductPort {
     private final ProductRepository productRepository;
 
--- src/main/java/com/example/demo/ProductService.java
+++ src/main/java/com/example/demo/ProductService.java
@@ -1,5 +1,19 @@
 package com.example.demo;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.beans.Transient;
+
+@RestController
+@RequestMapping("/products")
 class ProductService {
     private final ProductPort productPort;
 
@@ -7,8 +21,11 @@ class ProductService {
         this.productPort = productPort;
     }
 
-    public void addProduct(AddProductRequest request) {
+    @PostMapping
+    public ResponseEntity<Void> addProduct(@RequestBody AddProductRequest request) {
         Product product = new Product(request.name(), request.price(), request.discountPolicy());
         productPort.save(product);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
